e73dbda0d2aa6f357bc845f9bb59b5dbd5ec954f
spinningideas <scott@spinningideas.com>
2020-09-26 16:40:09 -0600

    remove unused param

--- src/main/java/com/sbpa/controllers/ContinentsController.java
+++ src/main/java/com/sbpa/controllers/ContinentsController.java
@@ -7,14 +7,13 @@ import com.sbpa.models.Continent;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContinentsController {
 
 	@GetMapping("/continents")
-	public ResponseEntity<List<Continent>> continents(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public ResponseEntity<List<Continent>> continents() {
 		ContinentsRepository repo = new ContinentsRepository();
 		List<Continent> continents = repo.GetContinents();
 		return ResponseEntity.ok(continents);
